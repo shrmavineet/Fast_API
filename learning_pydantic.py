8b5e600b7ce2ae79126a795dc83d27fcf4312d60
shrmavineet <shrmavineet8@gmail.com>
2026-09-16 05:59:55 +0000

    Inprogress pydantic validator

---
+++ learning_pydantic.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel,EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
+
+class Patient(BaseModel):
+    name: Annotated[str, Field(max_length=50, title='Name of the patient', description='Name of the patient which lenght is not more than 50 character', examples=['Vineet','Vivek'])]
+    age: int = Field(gt=0, lt=120, default=18)
+    email: EmailStr
+    linkedin_url: AnyUrl
+    weight: Annotated[float, Field(gt=0, strict=True)]
+    married: Optional[bool] = None
+    allergies: List[str]
+    conact_details: Dict[str, str]
+
+def insert_patient_data(patient: Patient):
+    print(patient.name)
+    print(patient.age)
+    print(f'Patient Added in db')
+
+patient_info = {'name':'Vineet', 'age':30, 'email':'abc@gmail.com','linkedin_url':'https://linkedin.com/132' ,'weight': 75.2, 'allergies':['pollen', 'dust'], 'conact_details':{'contact_no':'1234567'}}
+
+patient1 = Patient(**patient_info)
+
+insert_patient_data(patient1)
